652823840d541e1e7fa33ade715240e5c2ea2225
Rafael Moura <rafaelcostamoura@gmail.com>
2021-08-10 06:16:48 -0300

    componentizando

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,43 @@
-export function SideBar() {
-  // Complete aqui
+import * as React from "react";
+import { Button } from "./Button";
+
+import "../styles/sidebar.scss";
+
+interface SideBarProps {
+  genres?: [
+    {
+      id: number;
+      name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+      title: string;
+    }
+  ];
+  clickButton: Function;
+  selectedGenreId: number;
+}
+
+export function SideBar({
+  genres,
+  clickButton,
+  selectedGenreId,
+}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres &&
+          genres.map((genre) => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => clickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+      </div>
+    </nav>
+  );
 }
